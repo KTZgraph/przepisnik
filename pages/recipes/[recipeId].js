9a55618a02638fd621852350d99926a8a4b12f31
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-06 15:17:00 +0100

    poczatkowa struktura strony

---
+++ pages/recipes/[recipeId].js
@@ -0,0 +1,11 @@
+// konkrenty przepis ze szególami
+
+function RecipeDetailPage() {
+    return (
+      <div>
+        <h1> Recipe Detail</h1>
+      </div>
+    );
+  }
+  
+  export default RecipeDetailPage;
---
+++ pages/recipes/index.js
@@ -0,0 +1,12 @@
+// wszystkie przpisy
+
+function AllRecipesPage() {
+    return (
+      <div>
+        <h1>All Recipes</h1>
+      </div>
+    );
+  }
+  
+  export default AllRecipesPage;
+  
