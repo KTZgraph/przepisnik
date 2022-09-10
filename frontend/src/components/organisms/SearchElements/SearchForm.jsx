3ffbc3788a403082eb6ed642243aa1e18c5d48fc
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-09-10 23:55:35 +0200

    chowanie części opcji menu

--- frontend/src/components/organisms/SearchElements/SearchForm.jsx
+++ frontend/src/components/organisms/SearchElements/SearchForm.jsx
@@ -1,8 +1,11 @@
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import "./SearchForm.scss";
 
 const SearchForm = () => {
+  const [expanded, setExpanded] = useState(false);
+
   const keywordRef = useRef();
   const familyRef = useRef();
   const addedByRef = useRef();
@@ -13,6 +16,10 @@ const SearchForm = () => {
   const vulnerabilityDateBetweenStartRef = useRef();
   const vulnerabilityDateBetweenEndRef = useRef();
   const gitStatusRef = useRef();
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -44,95 +51,120 @@ const SearchForm = () => {
     console.log("status:", status);
   };
 
-  return (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <div className="row">
-        {/* pierwszy wiersz */}
-        <div className="form__input">
-          {/* TODO NIE Dropdown */}
-          <label htmlFor="keyword">Keyword:</label>
-          <input type="text" id="keyword" name="keyword" ref={keywordRef} />
+  if (!expanded) {
+    return (
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="options">
+          <ExpandMoreIcon className="icon" onClick={handleClick} />
         </div>
-        <div className="form__input">
-          <label htmlFor="family">Family:</label>
-          <input type="text" id="family" name="family" ref={familyRef} />
-        </div>
-      </div>
-      {/* drugi wiersz */}
-      <div className="row">
-        <div className="form__input">
-          <label htmlFor="addedBy">Added By:</label>
-          <input type="text" id="addedBy" name="addedBy" ref={addedByRef} />
-        </div>
-        <div className="form__input">
-          <label htmlFor="QABy">QA By:</label>
-          <input type="text" id="QABy" name="QABy" ref={QAByRef} />
-        </div>
-        <div className="form__input">
-          <label htmlFor="status">Status:</label>
-          <input type="text" id="status" name="status" ref={statusRef} />
-        </div>
-      </div>
-      {/* trzeci wiersz */}
-      <div className="row">
-        <div className="form__input">
-          <label htmlFor="creationDateBetween">Creation date between:</label>
-          {/* TODO NIE Dropdown */}
-          <div className="input-multiple">
-            <input
-              type="text"
-              id="creationDateBetweenStart"
-              name="creationDateBetweenStart"
-              ref={creationDateBetweenStartRef}
-            />
+        <div className="row">
+          {/* pierwszy wiersz */}
+          <div className="form__input">
             {/* TODO NIE Dropdown */}
-            <input
-              type="text"
-              id="creationDateBetweenEnd"
-              name="creationDateBetweenEnd"
-              ref={creationDateBetweenEndRef}
-            />
+            <label htmlFor="keyword">Keyword:</label>
+            <input type="text" id="keyword" name="keyword" ref={keywordRef} />
+          </div>
+          <div className="form__input">
+            <label htmlFor="family">Family:</label>
+            <input type="text" id="family" name="family" ref={familyRef} />
           </div>
         </div>
-        <div className="form__input">
-          <label htmlFor="vulnerabilityDateBetween">
-            Vulnerability date between:
-          </label>
-          {/* TODO NIE Dropdown */}
-          <div className="input-multiple">
-            <input
-              type="text"
-              id="vulnerabilityDateBetweenStart"
-              name="vulnerabilityDateBetweenStart"
-              ref={vulnerabilityDateBetweenStartRef}
-            />
+      </form>
+    );
+  } else {
+    return (
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="options">
+          <ExpandLessIcon className="icon" onClick={handleClick} />
+        </div>
+        <div className="row">
+          {/* pierwszy wiersz */}
+          <div className="form__input">
             {/* TODO NIE Dropdown */}
-            <input
-              type="text"
-              id="vulnerabilityDateBetweenEnd"
-              name="vulnerabilityDateBetweenEnd"
-              ref={vulnerabilityDateBetweenEndRef}
-            />
+            <label htmlFor="keyword">Keyword:</label>
+            <input type="text" id="keyword" name="keyword" ref={keywordRef} />
+          </div>
+          <div className="form__input">
+            <label htmlFor="family">Family:</label>
+            <input type="text" id="family" name="family" ref={familyRef} />
           </div>
         </div>
-      </div>
-      {/* czwarty wiersz */}
-      <div className="row">
-        <div className="form__input">
-          <label htmlFor="gitStatus">Git status:</label>
-          <input
-            type="text"
-            id="gitStatus"
-            name="gitStatus"
-            ref={gitStatusRef}
-          />
+        {/* drugi wiersz */}
+        <div className="row">
+          <div className="form__input">
+            <label htmlFor="addedBy">Added By:</label>
+            <input type="text" id="addedBy" name="addedBy" ref={addedByRef} />
+          </div>
+          <div className="form__input">
+            <label htmlFor="QABy">QA By:</label>
+            <input type="text" id="QABy" name="QABy" ref={QAByRef} />
+          </div>
+          <div className="form__input">
+            <label htmlFor="status">Status:</label>
+            <input type="text" id="status" name="status" ref={statusRef} />
+          </div>
         </div>
-        <button type="submit" className="action">
-          Search
-        </button>
-      </div>
-    </form>
-  );
+        {/* trzeci wiersz */}
+        <div className="row">
+          <div className="form__input">
+            <label htmlFor="creationDateBetween">Creation date between:</label>
+            {/* TODO NIE Dropdown */}
+            <div className="input-multiple">
+              <input
+                type="text"
+                id="creationDateBetweenStart"
+                name="creationDateBetweenStart"
+                ref={creationDateBetweenStartRef}
+              />
+              {/* TODO NIE Dropdown */}
+              <input
+                type="text"
+                id="creationDateBetweenEnd"
+                name="creationDateBetweenEnd"
+                ref={creationDateBetweenEndRef}
+              />
+            </div>
+          </div>
+          <div className="form__input">
+            <label htmlFor="vulnerabilityDateBetween">
+              Vulnerability date between:
+            </label>
+            {/* TODO NIE Dropdown */}
+            <div className="input-multiple">
+              <input
+                type="text"
+                id="vulnerabilityDateBetweenStart"
+                name="vulnerabilityDateBetweenStart"
+                ref={vulnerabilityDateBetweenStartRef}
+              />
+              {/* TODO NIE Dropdown */}
+              <input
+                type="text"
+                id="vulnerabilityDateBetweenEnd"
+                name="vulnerabilityDateBetweenEnd"
+                ref={vulnerabilityDateBetweenEndRef}
+              />
+            </div>
+          </div>
+        </div>
+        {/* czwarty wiersz */}
+        <div className="row">
+          <div className="form__input">
+            <label htmlFor="gitStatus">Git status:</label>
+            <input
+              type="text"
+              id="gitStatus"
+              name="gitStatus"
+              ref={gitStatusRef}
+            />
+          </div>
+          <button type="submit" className="action">
+            Search
+          </button>
+        </div>
+      </form>
+    );
+  }
 };
 
 export default SearchForm;
